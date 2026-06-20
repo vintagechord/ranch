41335f8babb016304549f2555fa0fb3740c5378f
vintagechord85 <vintagechord85@gmail.com>
2026-06-20 21:15:34 +0900

    Make header piggy bank cuter

--- app/components/PiggyBankButton.tsx
+++ app/components/PiggyBankButton.tsx
@@ -109,10 +109,17 @@ export default function PiggyBankButton() {
         aria-expanded={isOpen}
         onClick={() => setIsOpen((current) => !current)}
       >
-        <svg viewBox="0 0 28 28" aria-hidden="true" focusable="false">
-          <path d="M8.4 12.1c.9-2.4 3.4-4.1 6.6-4.1h4.3c.9 0 1.7.2 2.4.6l2.5-1.7c.5-.3 1.1 0 1.1.6v4.3c.5.8.8 1.7.8 2.8 0 2.6-1.7 4.8-4.3 5.8v2.1c0 .6-.5 1.1-1.1 1.1h-2.1c-.6 0-1.1-.5-1.1-1.1v-1.2h-6v1.2c0 .6-.5 1.1-1.1 1.1H8.3c-.6 0-1.1-.5-1.1-1.1v-2.1c-1.2-.6-2.1-1.5-2.7-2.5H2.7c-.6 0-1.1-.5-1.1-1.1v-2.3c0-.6.5-1.1 1.1-1.1h1.2c.8-.6 2.8-.9 4.5-1.3Z" />
-          <path d="M12.2 7.5c.6-1.8 2.1-3 4-3 1 0 1.8.3 2.6.9" />
-          <circle cx="21.5" cy="13.1" r="1" />
+        <svg viewBox="0 0 36 36" aria-hidden="true" focusable="false">
+          <path className="piggy-tail" d="M8.3 17.2c-3.2-.6-4.3-3.8-2.3-5.1 1.6-1 3.5.3 3.1 1.9-.3 1.1-1.5 1.4-2.3.6" />
+          <path className="piggy-leg" d="M12.2 24.5v4.2h3.8v-3" />
+          <path className="piggy-leg" d="M23.8 24.7v4h3.8v-4.2" />
+          <path className="piggy-body" d="M8.7 17.5c.6-5 5.1-8.1 11.4-8.1h2.4c5.6 0 9.8 3.5 9.8 8.2 0 4.9-4.4 8.7-10.6 8.7h-5.8c-4.6 0-8.2-2.2-9.5-5.5H4.7c-.7 0-1.2-.5-1.2-1.2v-2c0-.7.5-1.2 1.2-1.2h2.1c.5-.4 1.1-.7 1.9-.9Z" />
+          <path className="piggy-ear" d="M23.8 9.7 27.7 6c.5-.5 1.4-.1 1.3.6l-.7 5.2" />
+          <path className="piggy-slot" d="M15.2 8.7h7" />
+          <ellipse className="piggy-snout" cx="30.4" cy="17.5" rx="3.1" ry="2.7" />
+          <circle className="piggy-eye" cx="25.5" cy="15.2" r="1" />
+          <path className="piggy-nostril" d="M29.5 17.2v.1M31.4 17.2v.1" />
+          <path className="piggy-coin" d="M17.9 4.7a2 2 0 1 0 4 0 2 2 0 0 0-4 0Z" />
         </svg>
       </button>
 
