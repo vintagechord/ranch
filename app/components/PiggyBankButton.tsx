52c0cbbe21b7360ab3a7b6084ef27184394a6249
vintagechord85 <vintagechord85@gmail.com>
2026-08-20 00:42:43 +0900

    style: redesign piggy bank icon

--- app/components/PiggyBankButton.tsx
+++ app/components/PiggyBankButton.tsx
@@ -131,16 +131,16 @@ export default function PiggyBankButton() {
         onClick={() => setIsOpen((current) => !current)}
       >
         <svg viewBox="0 0 36 36" aria-hidden="true" focusable="false">
-          <path className="piggy-tail" d="M8.3 17.2c-3.2-.6-4.3-3.8-2.3-5.1 1.6-1 3.5.3 3.1 1.9-.3 1.1-1.5 1.4-2.3.6" />
-          <path className="piggy-leg" d="M12.2 24.5v4.2h3.8v-3" />
-          <path className="piggy-leg" d="M23.8 24.7v4h3.8v-4.2" />
-          <path className="piggy-body" d="M8.7 17.5c.6-5 5.1-8.1 11.4-8.1h2.4c5.6 0 9.8 3.5 9.8 8.2 0 4.9-4.4 8.7-10.6 8.7h-5.8c-4.6 0-8.2-2.2-9.5-5.5H4.7c-.7 0-1.2-.5-1.2-1.2v-2c0-.7.5-1.2 1.2-1.2h2.1c.5-.4 1.1-.7 1.9-.9Z" />
-          <path className="piggy-ear" d="M23.8 9.7 27.7 6c.5-.5 1.4-.1 1.3.6l-.7 5.2" />
-          <path className="piggy-slot" d="M15.2 8.7h7" />
-          <ellipse className="piggy-snout" cx="30.4" cy="17.5" rx="3.1" ry="2.7" />
-          <circle className="piggy-eye" cx="25.5" cy="15.2" r="1" />
-          <path className="piggy-nostril" d="M29.5 17.2v.1M31.4 17.2v.1" />
-          <path className="piggy-coin" d="M17.9 4.7a2 2 0 1 0 4 0 2 2 0 0 0-4 0Z" />
+          <path className="piggy-tail" d="M8.6 16.4c-2.3-2.4-4.9-1.3-4.5 1 .3 1.7 2.4 2.2 3.8.5" />
+          <path className="piggy-leg" d="M11.6 23.2h4.3v5.4h-4.3ZM22.8 23.2h4.3v5.4h-4.3Z" />
+          <path className="piggy-ear" d="M22.9 10.2 27.2 6.4l.9 6.5Z" />
+          <path className="piggy-body" d="M7.5 17.8c0-5.4 4.6-8.7 11.5-8.7h2.4c5.9 0 9.9 3.4 9.9 8.7s-4.3 8.7-10.6 8.7h-3c-6.3 0-10.2-3.4-10.2-8.7Z" />
+          <ellipse className="piggy-snout" cx="31.3" cy="18" rx="3.1" ry="2.7" />
+          <path className="piggy-slot" d="M12.9 10.2h11.6" />
+          <circle className="piggy-eye" cx="26" cy="15.1" r="1.1" />
+          <circle className="piggy-nostril" cx="31.8" cy="18" r="0.75" />
+          <circle className="piggy-coin" cx="18.7" cy="7" r="3.1" />
+          <path className="piggy-coin-mark" d="M18.7 5.4v3.2" />
         </svg>
       </button>
 
